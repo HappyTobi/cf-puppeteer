6f6be1cd70ad32155fe297ae0496d28d8083929f
HappyTobi <happytobi@tscoding.de>
2019-05-08 21:37:21 +0200

    [NEW] add v2 route api

--- v2cfResources/v2cfResources.go
+++ v2cfResources/v2cfResources.go
@@ -14,6 +14,7 @@ import (
 type V2CfResourcesInterface interface {
 	LoadAppRoutes(appGUID string) (*V2AppRoutesResponse, error)
 	LoadSharedDomains(domainGUID string) (*V2SharedDomainResponse, error)
+	CreateRoute(spaceGUID string, domainGUID string, host string) (*V2RouteResponse, error)
 }
 
 //ResourcesData struct to hold important instances to run push
@@ -141,6 +142,77 @@ func (resource *ResourcesData) LoadSharedDomains(domainGUID string) (*V2SharedDo
 	}
 
 	var response V2SharedDomainResponse
+	err = json.Unmarshal([]byte(jsonResp), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+type V2Route struct {
+	DomainGUID string `json:"domain_guid"`
+	SpaceGUID  string `json:"space_guid"`
+	Port       int    `json:"port,omitempty"`
+	Host       string `json:"host,omitempty"`
+}
+
+//V2RouteResponse
+type V2RouteResponse struct {
+	Metadata struct {
+		GUID      string    `json:"guid"`
+		URL       string    `json:"url"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	} `json:"metadata"`
+	Entity struct {
+		Host                string      `json:"host"`
+		Path                string      `json:"path"`
+		DomainGUID          string      `json:"domain_guid"`
+		SpaceGUID           string      `json:"space_guid"`
+		ServiceInstanceGUID interface{} `json:"service_instance_guid"`
+		Port                int         `json:"port"`
+		DomainURL           string      `json:"domain_url"`
+		SpaceURL            string      `json:"space_url"`
+		AppsURL             string      `json:"apps_url"`
+		RouteMappingsURL    string      `json:"route_mappings_url"`
+	} `json:"entity"`
+}
+
+//LoadSharedDomains
+func (resource *ResourcesData) CreateRoute(spaceGUID string, domainGUID string, host string) (*V2RouteResponse, error) {
+	path := fmt.Sprintf(`/v2/routes`)
+
+	var v2Route V2Route
+	v2Route.DomainGUID = domainGUID
+	v2Route.SpaceGUID = spaceGUID
+	v2Route.Host = host
+
+	appJSON, err := json.Marshal(v2Route)
+	if err != nil {
+		return nil, err
+	}
+
+	if resource.TraceLogging {
+		fmt.Printf("send POST to route: %s with body:\n", path)
+		prettyPrintJSON(string(appJSON))
+	}
+
+	result, err := resource.Connection.CliCommandWithoutTerminalOutput("curl", path, "-X", "POST", "-H", "Content-type: application/json", "-d",
+		string(appJSON))
+
+	if err != nil {
+		return nil, err
+	}
+
+	jsonResp := strings.Join(result, "")
+
+	if resource.TraceLogging {
+		fmt.Printf("response from call to path: %s was:\n", path)
+		prettyPrintJSON(jsonResp)
+	}
+
+	var response V2RouteResponse
 	err = json.Unmarshal([]byte(jsonResp), &response)
 	if err != nil {
 		return nil, err
