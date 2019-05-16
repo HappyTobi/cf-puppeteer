a54d947561d043bb08f9482f28f1c8450dbbfe7c
HappyTobi <happytobi@tscoding.de>
2019-05-16 23:59:03 +0200

    [NEW] add ui wrapper to print informations

---
+++ ui/ui.go
@@ -0,0 +1,38 @@
+package ui
+
+import (
+	"os"
+
+	"code.cloudfoundry.org/cli/cf/terminal"
+	"code.cloudfoundry.org/cli/cf/trace"
+)
+
+var ui terminal.UI
+
+func init() {
+	ui = terminal.NewUI(
+		os.Stdin,
+		os.Stdout,
+		terminal.NewTeePrinter(os.Stdout),
+		trace.NewLogger(os.Stdout, false, "", ""))
+}
+
+//Say message see cf/terminal
+func Say(message string, args ...interface{}) {
+	ui.Say(message, args...)
+}
+
+//Ok message see cf/terminal
+func Ok() {
+	ui.Ok()
+}
+
+//Failed message see cf/terminal
+func Failed(message string, args ...interface{}) {
+	ui.Failed(message, args...)
+}
+
+//LoadingIndication message see cf/terminal
+func LoadingIndication() {
+	ui.LoadingIndication()
+}
