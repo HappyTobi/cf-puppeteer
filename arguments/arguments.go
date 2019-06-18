598722b07c1bdaab85bc082744d39ca2ef431dc8
HappyTobi <happytobi@tscoding.de>
2019-06-18 07:10:00 +0200

    [NEW] refactor arguments parser

---
+++ arguments/arguments.go
@@ -0,0 +1,166 @@
+package arguments
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/happytobi/cf-puppeteer/manifest"
+	"regexp"
+	"strings"
+)
+
+//ParserArguments struct where all arguments will be parsed into
+type ParserArguments struct {
+	AppName                 string
+	ManifestPath            string
+	AppPath                 string
+	HealthCheckType         string
+	HealthCheckHTTPEndpoint string
+	Timeout                 int
+	InvocationTimeout       int
+	Process                 string
+	StackName               string
+	VendorAppOption         string
+	Envs                    []string
+	ShowLogs                bool
+	ShowCrashLogs           bool
+	DockerImage             string
+	DockerUserName          string
+	Manifest                manifest.Manifest
+	MergedEnvs              []string
+	LegacyPush              bool
+}
+
+type stringSlice []string
+
+func (s *stringSlice) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *stringSlice) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+var (
+	//ErrNoArgument error when zero-downtime-push without a argument called
+	ErrNoArgument = errors.New("no valid argument found, use --help / -h for more information")
+	//ErrNoManifest error when manifes on push application was not found
+	ErrNoManifest = errors.New("a manifest is required to push this application")
+	//ErrWrongEnvFormat error when env files was not in right format
+	ErrWrongEnvFormat = errors.New("--var would be in wrong format, use the vars like key=value")
+	//ErrWrongCombination error when legacy push is used with health check options
+	ErrWrongCombination = errors.New("--legacy-push and health check options coiuldn't be combined")
+)
+
+// ParseArgs parses the command line arguments
+func ParseArgs(args []string) (*ParserArguments, error) {
+	flags := flag.NewFlagSet("zero-downtime-push", flag.ContinueOnError)
+
+	var envs stringSlice
+
+	pta := &ParserArguments{}
+	flags.StringVar(&pta.ManifestPath, "f", "", "path to an application manifest")
+	flags.StringVar(&pta.AppPath, "p", "", "path to application files")
+	flags.StringVar(&pta.StackName, "s", "", "name of the stack to use")
+	flags.StringVar(&pta.HealthCheckType, "health-check-type", "", "type of health check to perform")
+	flags.StringVar(&pta.HealthCheckHTTPEndpoint, "health-check-http-endpoint", "", "endpoint for the 'http' health check type")
+	flags.IntVar(&pta.Timeout, "t", 0, "push timeout in seconds (defaults to 60 seconds)")
+	flags.IntVar(&pta.InvocationTimeout, "invocation-timeout", -1, "health check invocation timeout in seconds")
+	flags.StringVar(&pta.Process, "process", "", "application process to update")
+	flags.BoolVar(&pta.ShowLogs, "show-app-log", false, "tail and show application log during application start")
+	flags.BoolVar(&pta.ShowCrashLogs, "show-crash-log", false, "Show recent logs when applications crashes while the deployment")
+	flags.StringVar(&pta.VendorAppOption, "vendor-option", "delete", "option to delete or stop vendor application - default is delete")
+	flags.Var(&envs, "env", "Variable key value pair for adding dynamic environment variables; can specify multiple times")
+	flags.BoolVar(&pta.LegacyPush, "legacy-push", false, "use legacy push instead of new v3 api")
+	//flags.StringVar(&pta.DockerImage, "docker-image", "", "url to docker image")
+	//flags.StringVar(&pta.DockerUserName, "docker-username", "", "pass docker username if image came from private repository")
+	//dockerPass := os.Getenv("CF_DOCKER_PASSWORD")
+
+	//first check if argument was passed
+	if len(args) < 2 {
+		return pta, ErrNoArgument
+	}
+
+	//default start index of parameters is 2 because 1 is the appName
+	argumentStartIndex := 2
+	//if first argument is not the appName we have to read the appName out of the manifest
+	noAppNameProvided, _ := regexp.MatchString("^-[a-z]{0,3}", args[1])
+	//noAppNameProvided := strings.Contains(args[1], "-")
+	if noAppNameProvided {
+		argumentStartIndex = 1
+	}
+
+	err := flags.Parse(args[argumentStartIndex:])
+	if err != nil {
+		return pta, err
+	}
+
+	if pta.ManifestPath == "" {
+		return pta, ErrNoManifest
+	}
+
+	//parse manifest
+	parsedManifest, err := manifest.Parse(pta.ManifestPath)
+	if err != nil {
+		return pta, err //ErrManifest
+	}
+	pta.Manifest = parsedManifest
+
+	/*if *dockerImage != "" && *dockerUserName != "" && dockerPass != "" {
+	    //TODO use dockerImage stuff and pass to push command
+	}*/
+
+	//set timeout
+	manifestTimeout := parsedManifest.ApplicationManifests[0].Timeout
+	if manifestTimeout > 0 && pta.Timeout <= 0 {
+		pta.Timeout = manifestTimeout
+	} else if manifestTimeout <= 0 && pta.Timeout <= 0 {
+		pta.Timeout = 60
+	}
+
+	//parse first argument as appName
+	pta.AppName = args[1]
+	if noAppNameProvided {
+		pta.AppName = parsedManifest.ApplicationManifests[0].Name
+	}
+
+	//check that health check works without legacy push only
+	if pta.LegacyPush && (pta.HealthCheckType != "" || pta.HealthCheckHTTPEndpoint != "") {
+		return nil, ErrWrongCombination
+	}
+
+	// get health check settings from manifest if nothing else was specified in the command line
+	if pta.HealthCheckType == "" {
+		if parsedManifest.ApplicationManifests[0].HealthCheckType == "" {
+			pta.HealthCheckType = "port"
+		} else {
+			pta.HealthCheckType = parsedManifest.ApplicationManifests[0].HealthCheckType
+		}
+
+	}
+	if pta.HealthCheckHTTPEndpoint == "" {
+		pta.HealthCheckHTTPEndpoint = parsedManifest.ApplicationManifests[0].HealthCheckHTTPEndpoint
+	}
+
+	//validate envs format
+	if len(envs) > 0 {
+		for _, envPair := range envs {
+			if strings.Contains(envPair, "=") == false {
+				return pta, ErrWrongEnvFormat
+			}
+		}
+	}
+
+	pta.Envs = envs
+
+	//Merge all environment variables together
+	var mergedEnvs []string
+	mergedEnvs = append(mergedEnvs, pta.Envs...)
+	for k, v := range pta.Manifest.ApplicationManifests[0].Env {
+		mergedEnvs = append(mergedEnvs, fmt.Sprintf("%s=%s", k, v))
+	}
+	pta.MergedEnvs = mergedEnvs
+
+	return pta, nil
+}
---
+++ arguments/arguments_test.go
@@ -0,0 +1,192 @@
+package arguments
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestArgumentsParser(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Arguments Parser TestSuite")
+}
+
+var _ = Describe("Flag Parsing", func() {
+	var ()
+
+	BeforeEach(func() {
+
+	})
+
+	It("parses args without appName", func() {
+		parsedArguments, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"-f", "../fixtures/manifest.yml",
+				"-p", "app-path",
+				"-s", "stack-name",
+				"-t", "120",
+				"-env", "foo=bar",
+				"-env", "baz=bob=true",
+				"--vendor-option", "stop",
+				"--invocation-timeout", "2211",
+				"--process", "process-name",
+			},
+		)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(parsedArguments.AppName).Should(Equal("myApp"))
+		Expect(parsedArguments.ManifestPath).To(Equal("../fixtures/manifest.yml"))
+		Expect(parsedArguments.AppPath).To(Equal("app-path"))
+		Expect(parsedArguments.StackName).To(Equal("stack-name"))
+		Expect(parsedArguments.Envs).To(Equal([]string{"foo=bar", "baz=bob=true"}))
+		Expect(parsedArguments.VendorAppOption).Should(Equal("stop"))
+		Expect(parsedArguments.ShowLogs).To(Equal(false))
+		Expect(parsedArguments.ShowCrashLogs).To(Equal(false))
+		Expect(parsedArguments.Timeout).To(Equal(120))
+		Expect(parsedArguments.InvocationTimeout).To(Equal(2211))
+		Expect(parsedArguments.Process).To(Equal("process-name"))
+		Expect(parsedArguments.HealthCheckType).To(Equal("http"))
+		Expect(parsedArguments.HealthCheckHTTPEndpoint).To(Equal("/health"))
+	})
+
+	It("parses a all args without timeout", func() {
+		parsedArguments, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"appname",
+				"-f", "../fixtures/manifest.yml",
+				"-p", "app-path",
+				"-s", "stack-name",
+				"-env", "foo=bar",
+				"-env", "baz=bob",
+				"--vendor-option", "stop",
+			},
+		)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(parsedArguments.AppName).To(Equal("appname"))
+		Expect(parsedArguments.ManifestPath).To(Equal("../fixtures/manifest.yml"))
+		Expect(parsedArguments.AppPath).To(Equal("app-path"))
+		Expect(parsedArguments.StackName).To(Equal("stack-name"))
+		Expect(parsedArguments.Envs).To(Equal([]string{"foo=bar", "baz=bob"}))
+		Expect(parsedArguments.VendorAppOption).Should(Equal("stop"))
+		Expect(parsedArguments.ShowLogs).To(Equal(false))
+		Expect(parsedArguments.ShowCrashLogs).To(Equal(false))
+		Expect(parsedArguments.Timeout).To(Equal(2))
+		Expect(parsedArguments.InvocationTimeout).To(Equal(-1))
+		Expect(parsedArguments.Process).To(Equal(""))
+		Expect(parsedArguments.HealthCheckType).To(Equal("http"))
+		Expect(parsedArguments.HealthCheckHTTPEndpoint).To(Equal("/health"))
+	})
+
+	It("parses a all args without timeout and no manifest timeout", func() {
+		parsedArguments, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"appname",
+				"-f", "../fixtures/multiManifest.yml",
+				"-p", "app-path",
+				"-s", "stack-name",
+				"-env", "foo=bar",
+				"-env", "baz=bob",
+				"--vendor-option", "stop",
+				"--show-crash-log",
+			},
+		)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(parsedArguments.AppName).To(Equal("appname"))
+		Expect(parsedArguments.ManifestPath).To(Equal("../fixtures/multiManifest.yml"))
+		Expect(parsedArguments.AppPath).To(Equal("app-path"))
+		Expect(parsedArguments.StackName).To(Equal("stack-name"))
+		Expect(parsedArguments.Envs).To(Equal([]string{"foo=bar", "baz=bob"}))
+		Expect(parsedArguments.VendorAppOption).Should(Equal("stop"))
+		Expect(parsedArguments.ShowLogs).To(Equal(false))
+		Expect(parsedArguments.ShowCrashLogs).To(Equal(true))
+		Expect(parsedArguments.Timeout).To(Equal(60))
+		Expect(parsedArguments.InvocationTimeout).To(Equal(-1))
+		Expect(parsedArguments.Process).To(Equal(""))
+		Expect(parsedArguments.HealthCheckType).To(Equal("http"))
+		Expect(parsedArguments.HealthCheckHTTPEndpoint).To(Equal("/health"))
+	})
+
+	It("parses a complete set of args", func() {
+		parsedArguments, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"appname",
+				"-f", "../fixtures/manifest.yml",
+				"-p", "app-path",
+				"-s", "stack-name",
+				"-t", "120",
+				"-env", "foo=bar",
+				"-env", "baz=bob",
+				"--invocation-timeout", "2211",
+				"--process", "process-name",
+				"--health-check-type", "process",
+				"--health-check-http-endpoint", "/foo/bar",
+				"--show-app-log",
+			},
+		)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(parsedArguments.AppName).To(Equal("appname"))
+		Expect(parsedArguments.ManifestPath).To(Equal("../fixtures/manifest.yml"))
+		Expect(parsedArguments.AppPath).To(Equal("app-path"))
+		Expect(parsedArguments.StackName).To(Equal("stack-name"))
+		Expect(parsedArguments.Envs).To(Equal([]string{"foo=bar", "baz=bob"}))
+		Expect(parsedArguments.VendorAppOption).Should(Equal("delete"))
+		Expect(parsedArguments.ShowLogs).To(Equal(true))
+		Expect(parsedArguments.ShowCrashLogs).To(Equal(false))
+		Expect(parsedArguments.Timeout).To(Equal(120))
+		Expect(parsedArguments.InvocationTimeout).To(Equal(2211))
+		Expect(parsedArguments.Process).To(Equal("process-name"))
+		Expect(parsedArguments.HealthCheckType).To(Equal("process"))
+		Expect(parsedArguments.HealthCheckHTTPEndpoint).To(Equal("/foo/bar"))
+	})
+
+	It("parses args without appName and wrong envs format", func() {
+		_, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"-f", "../fixtures/manifest.yml",
+				"-p", "app-path",
+				"-s", "stack-name",
+				"-t", "120",
+				"-env", "foo=bar",
+				"-env", "baz bob",
+				"--invocation-timeout", "2211",
+				"--process", "process-name",
+			},
+		)
+		Expect(err).To(MatchError(ErrWrongEnvFormat))
+	})
+
+	It("requires a manifest", func() {
+		_, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"appname",
+				"-p", "app-path",
+			},
+		)
+		Expect(err).To(MatchError(ErrNoManifest))
+	})
+
+	It("legacy push with health check option", func() {
+		_, err := ParseArgs(
+			[]string{
+				"zero-downtime-push",
+				"appname",
+				"-f", "../fixtures/manifest.yml",
+				"-p", "app-path",
+				"--legacy-push",
+				"--health-check-type", "process",
+				"--health-check-http-endpoint", "/foo/bar",
+			},
+		)
+		Expect(err).To(MatchError(ErrWrongCombination))
+	})
+})
