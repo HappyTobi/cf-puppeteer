69d4ce0e854163f40b1d90e4778ca9ed3a48877b
HappyTobi <happytobi@tscoding.de>
2019-06-18 07:11:09 +0200

    [UPDATE] add post form

--- cf/cli/http.go
+++ cf/cli/http.go
@@ -15,6 +15,7 @@ import (
 //Calls interface
 type HttpCalls interface {
 	PostJSON(path string, body []byte) (string, error)
+	PostFormData(path string, body []byte, contentType string) (string, error)
 }
 
 //HttpConnection
@@ -49,6 +50,51 @@ func setupHttpClient(timout time.Duration, skipSSLValidation bool) *http.Client 
 	}
 
 	return &http.Client{Transport: tr}
+}
+
+func (conn *HttpConnection) PostFormData(path string, body []byte, contentType string) (string, error) {
+	request, err := http.NewRequest(
+		"POST",
+		path,
+		bytes.NewReader(body),
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	//get access token from cli connection
+	token, _ := conn.cliConnection.AccessToken()
+
+	request.Header = http.Header{}
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("User-Agent", "client.userAgent")
+	request.Header.Set("Content-Type", contentType)
+	request.Header.Set("Authorization", token)
+
+	if conn.traceLogging {
+		ui.Say("try to call path: %s", path)
+	}
+
+	res, err := conn.httpClient.Do(request)
+	if err != nil {
+		ui.Failed("Error while calling the apply manifest url %s - error: %s", path, err)
+		return "", err
+	}
+
+	result, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	jsonResp := string(result)
+	if conn.traceLogging {
+		if len(jsonResp) == 0 {
+			ui.Say("response from get call to path: %s was: %s", path, res.StatusCode)
+		} else {
+			ui.Say("response from get call to path: %s was: %s", path, print.PrettyJSON(jsonResp))
+		}
+	}
+
+	return jsonResp, nil
 }
 
 func (conn *HttpConnection) PostJSON(path string, body []byte) (string, error) {
