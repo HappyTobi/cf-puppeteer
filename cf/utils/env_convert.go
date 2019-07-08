3a9680ff3dc7156c1809009cb4ad003b1b4a11fb
HappyTobi <happytobi@tscoding.de>
2019-07-08 17:26:39 +0200

    [NEW] environment converter

---
+++ cf/utils/env_convert.go
@@ -0,0 +1,43 @@
+package print
+
+import (
+	"encoding/csv"
+	"github.com/happytobi/cf-puppeteer/ui"
+	"strings"
+)
+
+/*func Convert(envVars []string) (conVars map[string]string) {
+	envs := make(map[string]string)
+	for _, v := range envVars {
+		envPair := strings.Split(v, SplitChar)
+		envKey := strings.TrimSpace(envPair[0])
+		envVal := strings.TrimSpace(envPair[1])
+		envs[envKey] = envVal
+	}
+
+	return envs
+}*/
+
+func Convert(envVars []string) (conVars map[string]string) {
+	replacer := strings.NewReplacer("'", "\"", "`", "\"")
+	envs := make(map[string]string)
+	for _, v := range envVars {
+		fv := replacer.Replace(v)
+		r := csv.NewReader(strings.NewReader(fv))
+		r.Comma = '='
+		r.TrimLeadingSpace = true
+
+		fields, err := r.Read()
+		if err != nil {
+			ui.Warn("could not convert environment variable %s - error:%s", v, err)
+		}
+
+		if len(fields) > 1 {
+			envKey := strings.TrimSpace(fields[0])
+			envVal := strings.TrimSpace(fields[1])
+			envs[envKey] = envVal
+		}
+	}
+
+	return envs
+}
---
+++ cf/utils/env_convert_test.go
@@ -0,0 +1,51 @@
+package print_test
+
+import (
+	env_convert "github.com/happytobi/cf-puppeteer/cf/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestCfUtilEnvConverter(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Puppeteer CF Util Resources")
+}
+
+var _ = Describe("cf util environment converter", func() {
+	Describe("convert simple environment variables to map", func() {
+		It("convert simple", func() {
+			var simpleVars = []string{"foo=bar", "santa=claus"}
+			convertedEnvs := env_convert.Convert(simpleVars)
+			Expect(convertedEnvs["foo"]).To(Equal("bar"))
+			Expect(convertedEnvs["santa"]).To(Equal("claus"))
+			Expect(len(convertedEnvs)).To(Equal(2))
+		})
+	})
+
+	Describe("convert complex environment variables to map", func() {
+		It("convert complex", func() {
+			var simpleVars = []string{"jdbc=jdbc:oracle:thin:username/password@amrood:1521:EMP", "fstab=\"uid=1000,gid=100,umask=0,allow_other\""}
+			convertedEnvs := env_convert.Convert(simpleVars)
+			Expect(convertedEnvs["jdbc"]).To(Equal("jdbc:oracle:thin:username/password@amrood:1521:EMP"))
+			Expect(convertedEnvs["fstab"]).To(Equal("uid=1000,gid=100,umask=0,allow_other"))
+			Expect(len(convertedEnvs)).To(Equal(2))
+		})
+
+		It("convert complex with single quotes", func() {
+			var simpleVars = []string{"jdbc=jdbc:oracle:thin:username/password@amrood:1521:EMP", "fstab='uid=1000,gid=100,umask=0,allow_other'"}
+			convertedEnvs := env_convert.Convert(simpleVars)
+			Expect(convertedEnvs["jdbc"]).To(Equal("jdbc:oracle:thin:username/password@amrood:1521:EMP"))
+			Expect(convertedEnvs["fstab"]).To(Equal("uid=1000,gid=100,umask=0,allow_other"))
+			Expect(len(convertedEnvs)).To(Equal(2))
+		})
+	})
+
+	Describe("dont convert var's didn't match the pattern", func() {
+		It("convert non patter matching vars", func() {
+			var simpleVars = []string{"foo_bar", "santa-claus"}
+			convertedEnvs := env_convert.Convert(simpleVars)
+			Expect(len(convertedEnvs)).To(Equal(0))
+		})
+	})
+})
