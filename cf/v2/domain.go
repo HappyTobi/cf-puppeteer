787eb246a7bc684b4a67225cdafbd9bacf8a3d11
HappyTobi <happytobi@tscoding.de>
2019-11-20 22:40:25 +0100

    [UPDATE] v2 domain and route switch

---
+++ cf/v2/domain.go
@@ -0,0 +1,110 @@
+package v2
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type Routes struct {
+	Host   string
+	Domain string
+}
+
+//DomainResponse reponse while loading domains
+type DomainResponse struct {
+	Pagination struct {
+		TotalResults int    `json:"total_results"`
+		TotalPages   int    `json:"total_pages"`
+		NextUrl      string `json:"next_url"`
+		PrevUrl      string `json:"prev_url"`
+	} `json:"pagination"`
+	Resources []struct {
+		Metadata struct {
+			GUID string `json:"guid"`
+			Url  string `json:"url"`
+		} `json:"metadata"`
+		Entity struct {
+			Name     string `json:"name"`
+			Internal bool   `json:"internal"`
+		} `json:"entity"`
+	} `json:"resources"`
+}
+
+func (resource *LegacyResourcesData) GetDomain(domains []map[string]string) (*[]Routes, error) {
+	path := fmt.Sprintf(`/v2/domains`)
+
+	response, err := resource.getDomain(path)
+	if err != nil {
+		return nil, err
+	}
+
+	domainGUID := make(map[string]Routes)
+
+	for _, domainRes := range response.Resources {
+		for _, routes := range domains {
+			_, exists := domainGUID[domainRes.Metadata.GUID]
+			domain := routes["value"]
+			hostName := strings.ReplaceAll(domain, domainRes.Entity.Name, "")
+			if strings.Contains(domain, domainRes.Entity.Name) && len(hostName) > 0 && !exists {
+				hostName = strings.TrimRight(hostName, ".")
+				newRoute := &Routes{
+					Host:   hostName,
+					Domain: domainRes.Entity.Name,
+				}
+				domainGUID[domain] = *newRoute
+			}
+		}
+	}
+
+	for response.Pagination.NextUrl != "" && len(domainGUID) <= 0 {
+		response, err := resource.getDomain(response.Pagination.NextUrl)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, domainRes := range response.Resources {
+			for _, routes := range domains {
+				for _, domain := range routes {
+					_, exists := domainGUID[domain]
+					if strings.Contains(domain, domainRes.Entity.Name) && !exists {
+						hostName := strings.ReplaceAll(domain, domainRes.Entity.Name, "")
+						hostName = strings.TrimRight(hostName, ".")
+						newRoute := &Routes{
+							Host:   hostName,
+							Domain: domainRes.Entity.Name,
+						}
+						domainGUID[domain] = *newRoute
+					}
+				}
+			}
+		}
+	}
+
+	var domainsFound []Routes
+	for _, v := range domainGUID {
+		domainsFound = append(domainsFound, v)
+	}
+
+	return &domainsFound, err
+}
+
+func (resource *LegacyResourcesData) getDomain(path string) (*DomainResponse, error) {
+	var response DomainResponse
+
+	jsonResult, err := resource.Cli.GetJSON(path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(jsonResult), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	sort.Slice(response.Resources, func(i, j int) bool {
+		return response.Resources[i].Entity.Name < response.Resources[j].Entity.Name
+	})
+	return &response, nil
+}
